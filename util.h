44aced9c60065d1cd466687ebea38edca440ed9b
McSymilian <max.ryder8@gmail.com>
2024-04-16 12:49:10 +0200

    util.h filled

--- util.h
+++ util.h
@@ -4,3 +4,13 @@
 
 void DrawString(SDL_Surface* screen, int x, int y, const char* text,
 	SDL_Surface* charset);
+
+void DrawSurface(SDL_Surface* screen, SDL_Surface* sprite, int x, int y);
+
+void DrawPixel(SDL_Surface* surface, int x, int y, Uint32 color);
+
+void DrawLine(SDL_Surface* screen, int x, int y, int l, int dx, int dy, Uint32 color);
+
+void DrawRectangle(SDL_Surface* screen, int x, int y, int l, int k,
+	Uint32 outlineColor, Uint32 fillColor);
+
