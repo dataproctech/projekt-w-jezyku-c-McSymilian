05d67d90e2a9f15de17a2b1e49cf673de9f10def
McSymilian <max.ryder8@gmail.com>
2024-04-17 16:32:23 +0200

    rearrange files and directories

--- PawnsOnBoard.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-typedef int** Board;
-
-int getBoardSize();
-
-Board getPawnsOnBoard();
-
-void upadatePawnsOnBoard(Board updatedPawnsOnBoard);
---
+++ core/MainContoller.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<math.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+#include "../utility/Util.h"
+#include "../window/WindowParameters.h"
+
+void war_Caby_init_game();
