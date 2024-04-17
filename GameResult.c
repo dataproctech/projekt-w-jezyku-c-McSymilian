9e75ebebc28371b2d5a6330f7ea8d917a7bf9a08
McSymilian <max.ryder8@gmail.com>
2024-04-17 11:35:41 +0200

    generic game result creation

--- GameResult.c
+++ GameResult.c
@@ -4,3 +4,7 @@ MatchResultRecord* createMatchResultRecord(char* playerNameA, char* playerNameB,
 {
 	return &(MatchResultRecord) { .playerNameA = playerNameA, .playerNameB = playerNameB, .scoreA = scoreA, .scoreB = scoreB, .time = time };
 }
+
+MatchResultRecord* createGenericMatchResultRecord(bool doPlayerAWon, unsigned int time) {
+	return &(MatchResultRecord) { .playerNameA = "Player A", .playerNameB = "Player B", .scoreA = doPlayerAWon ? 1 : 0, .scoreB = doPlayerAWon ? 0 : 1, .time = time };
+}
--- GameResult.h
+++ GameResult.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <stdbool.h>
 /*
 Data model to handle and store game results
 */
@@ -16,3 +16,4 @@ typedef struct MatchResultRecord
 } MatchResultRecord;
 
 MatchResultRecord* createMatchResultRecord(char* playerNameA, char* playerNameB, char scoreA, char scoreB, unsigned int time);
+MatchResultRecord* createGenericMatchResultRecord(bool doPlayerAWon, unsigned int time);
