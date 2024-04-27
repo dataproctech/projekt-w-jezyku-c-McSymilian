e01f5441ea1d28c8e023b583ec96ac26a0f2f633
McSymilian <max.ryder8@gmail.com>
2024-04-28 00:45:05 +0200

    reading previous game state code cleaned

--- archive/GameStateRead.c
+++ archive/GameStateRead.c
@@ -9,16 +9,17 @@ Board readGameState() {
 		return NULL;
 	}
 
-	int** gameState = (int**)malloc(getBoardSize() * sizeof(int*));
+	Board gameState = (Board)malloc(getBoardSize() * sizeof(int*));
 
 	for (int i = 0; i < getBoardSize(); i++) {
 		gameState[i] = (int*)malloc(getBoardSize() * sizeof(int));
-		for (int j = 0; j < getBoardSize(); j++) {
+		for (int j = 0; j < getBoardSize(); j++) 
 			fscanf_s(file, "%d", &gameState[i][j]);
-			int a = gameState[i][j];
-		}
+		
 	}
+
 	fclose(file);
+
 	return gameState;
 
 }
